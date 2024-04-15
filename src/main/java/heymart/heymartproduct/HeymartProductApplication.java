ff81c690925f67a3d8751c812414e3bf37c35521
Will S <williamsusanto82@gmail.com>
2024-04-15 23:02:51 +0700

    Codebase Initialization

---
+++ src/main/java/heymart/heymartproduct/HeymartProductApplication.java
@@ -0,0 +1,13 @@
+package heymart.heymartproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeymartProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeymartProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/heymart/heymartproduct/HeymartProductApplicationTests.java
@@ -0,0 +1,13 @@
+package heymart.heymartproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeymartProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
