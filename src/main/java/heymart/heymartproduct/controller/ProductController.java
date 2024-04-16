f38f28137c67c092d382d7809274ccdd8469fe44
Will S <williamsusanto82@gmail.com>
2024-04-16 15:49:43 +0700

    Deployment testing 20

--- src/main/java/heymart/heymartproduct/controller/ProductController.java
+++ src/main/java/heymart/heymartproduct/controller/ProductController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("")
 public class ProductController {
-    @GetMapping("")
+    @GetMapping("/")
+    @ResponseBody
     public String mainPage(Model model) {
-        return "MainPage";
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
     }
 }
